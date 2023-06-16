f0abfd81478c698177f04d391de677b6a5427ea0
hesyboy <hesamahmoodi@gmail.com>
2023-06-17 02:56:27 +0330

    menu2

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-800 shadow-md py-2">
-    <div class="container mx-auto flex justify-between items-center">
+    <div class="container mx-auto flex flex-col gap-3 md:flex-row justify-between items-center">
 
         <div class="text-white font-semibold">
             Web Design | Web Application | ECommerce
@@ -23,23 +23,30 @@
     </div>
 
 </div>
-<div class="bg-white shadow-md py-2">
-    <div class="container mx-auto flex justify-between items-center">
+<div class="bg-white shadow-md py-2 px-2">
+    <div class="container mx-auto flex justify-between items-center ">
         <div>
-            <div class="text-2xl font-bold">
-                Web Shatel
+            <div class="text-2xl md:text-3xl font-semibold">
+                WEB SHATEL
             </div>
-            <div class="text-base font-semibold">
+            <div class="text-sm md:text-base font-semibold">
                 Digital Marketing Agancy
             </div>
         </div>
-        <div>
-            <ul class="flex gap-8 items-center justify-center font-semibold">
-                <li class=" text-indigo-800 font-bold ">
-                    Home
+        {{-- Menu Desktop --}}
+        <div class="hidden md:flex">
+            <ul class="flex gap-3 items-center justify-center font-semibold">
+                <li class="cursor-pointer  bg-gray-200 t px-4 py-2 rounded-md ">
+                    Design & Geraphic
+                </li>
+                <li class="cursor-pointer  bg-gray-200 t px-4 py-2 rounded-md ">
+                    Web Site & Application
+                </li>
+                <li class="cursor-pointer  bg-gray-200 t px-4 py-2 rounded-md ">
+                    Web Application
                 </li>
                 <li class=" text-gray-700 " x-data="{modal:false}">
-                    <div @click="modal=!modal" class="cursor-pointer  bg-gray-800 text-white px-4 py-2 rounded-md shadow-md">
+                    <div @click="modal=!modal" class="cursor-pointer  bg-gray-800 text-white px-4 py-2 rounded-md ">
                         Services
                     </div>
                     <div x-show="modal" x-transition x-cloak
@@ -52,11 +59,11 @@
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class="flex items-center gap-2">
                                             <span
-                                            class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
+                                            class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
                                                 <i class="fa-brands fa-figma"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     UI/UX
                                                 </span>
                                                 <span class="text-xs">
@@ -74,11 +81,11 @@
                                     <a @mouseover="tab=2" @mouseleave="tab=0"
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class="flex items-center gap-2">
-                                            <span class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
+                                            <span class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
                                                 <i class="fa-brands fa-wordpress-simple"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     Wordpress
                                                 </span>
                                                 <span class="text-xs">
@@ -96,11 +103,11 @@
                                     <a @mouseover="tab=3" @mouseleave="tab=0"
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class="flex items-center gap-2">
-                                            <span class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
+                                            <span class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
                                                 <i class="fa-brands fa-laravel"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     Laravel
                                                 </span>
                                                 <span class="text-xs">
@@ -118,11 +125,11 @@
                                     <a @mouseover="tab=4" @mouseleave="tab=0"
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class="flex items-center gap-2">
-                                            <span class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
+                                            <span class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
                                                 <i class="fa-solid fa-chart-line"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     Google SEO
                                                 </span>
                                                 <span class="text-xs">
@@ -140,11 +147,11 @@
                                     <a @mouseover="tab=5" @mouseleave="tab=0"
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class=" flex items-center gap-2">
-                                            <span class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
+                                            <span class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800 duration-200 group-hover:text-slate-100">
                                                 <i class="fa-brands fa-vuejs"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     Vue.JS
                                                 </span>
                                                 <span class="text-xs">
@@ -162,11 +169,11 @@
                                     <a @mouseover="tab=6" @mouseleave="tab=0"
                                      class="flex gap-5 justify-between items-center hover:bg-gray-100 p-2 rounded-lg duration-200" href="">
                                         <div class="flex items-center gap-2">
-                                            <span class="flex w-12 h-12 items-center justify-center shadow-md rounded-md text-3xl bg-white group-hover:bg-indigo-800  duration-200 group-hover:text-slate-100">
+                                            <span class="flex w-10 h-10 items-center justify-center shadow-md rounded-md text-2xl bg-white group-hover:bg-indigo-800  duration-200 group-hover:text-slate-100">
                                                 <i class="fa-brands fa-google"></i>
                                             </span>
                                             <h2 class="font-semibold  flex flex-col">
-                                                <span class="font-bold text-base">
+                                                <span class="font-bold text-sm">
                                                     Google Ads
                                                 </span>
                                                 <span class="text-xs">
@@ -182,8 +189,8 @@
 
 
                             </div>
-                            <div class="col-span-4  h-full flex items-center rounded-lg p-5">
-                                <div class="w-full flex flex-col gap-2 items-center justify-center" x-show="tab==0">
+                            <div class="col-span-4  h-full flex items-center rounded-lg py-6 px-3">
+                                <div class="w-full flex flex-col gap-4 items-center justify-center" x-show="tab==0" >
                                     <div class="text-3xl font-bold">
                                         WEB SHATEL
                                     </div>
@@ -205,7 +212,7 @@
                                     </div> --}}
                                 </div>
 
-                                <div  x-show="tab==1">
+                                <div  x-show="tab==1" >
                                     <div class="text-lg font-bold">
                                         What is UI/UX ?
                                     </div>
@@ -269,18 +276,27 @@
                         </div>
                     </div>
                 </li>
-                <li class=" ">
+                <li class="cursor-pointer  bg-gray-200 hover:bg-gray-300 t px-4 py-2 rounded-md ">
                     Blog
                 </li>
-                <li class="">
+                <li class="cursor-pointer  bg-gray-200 t px-4 py-2 rounded-md ">
                     About Us
                 </li>
-                <li class="  ">
+                <li class="cursor-pointer  bg-gray-200 t px-4 py-2 rounded-md ">
                     Contact Us
                 </li>
             </ul>
         </div>
-        <div class="flex gap-3 items-center">
+
+        {{-- Mobile Menu --}}
+
+        <div class="flex gap-1 items-center md:hidden">
+            {{-- <span class="font-semibold text-base">
+                Menu
+            </span> --}}
+            <i class="fa-solid fa-bars text-3xl"></i>
+        </div>
+        <div class="hidden md:flex gap-3 items-center">
             <a href="" class=" py-2 px-4 bg-gray-800 text-white rounded-md font-semibold">
                 Order Now
             </a>
